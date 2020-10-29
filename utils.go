7b9594d646541868e23296a45a4fa8c3cfbb71f1
Suwicha Khaosamlee <candleplans@gmail.com>
2020-10-29 17:52:13 +0700

    สร้างตัว Generate Schema สำหรับ Dgraph

--- utils.go
+++ utils.go
@@ -1,35 +1,74 @@
 package dgraphland
 
 import (
+	"bytes"
 	"context"
 	"reflect"
 	"strings"
 )
 
 const (
-	TagDgland = "dgland"
-	TagJson   = "json"
+	TagDgland     = "dgland"
+	TagJson       = "json"
+	ModelTypeName = "Model"
 )
 
+// ctx สำหรับค่า Context.Background()
 func ctx() context.Context {
 	return context.Background()
 }
 
-func Schema(model interface{}) string {
-	t := reflect.TypeOf(model)
+func Schema(models ...interface{}) string {
+	// แทรก Model{} เป็นรายการแรกใน Models []interface{}
+	models = append([]interface{}{&Model{}}, models...)
 
-	if t.NumField() == 0 {
-		return ""
-	}
+	bFields := bytes.Buffer{}     //สำหรับเก็บ Dgraph Field Buffer
+	bTypes := bytes.Buffer{}      // สำหรับเก็บ Dgraph Type  Buffer
+	bModelTypes := bytes.Buffer{} // สำหรับเก็บ Model Buffer
 
-	var strs []string
-	for i := 0; i < t.NumField(); i++ {
-		field := t.Field(i)
-		tagValueDglang := field.Tag.Get(TagDgland)
-		tagValueJson := field.Tag.Get(TagJson)
-		if tagValueDglang != "" && tagValueJson != "" {
-			strs = append(strs, field.Name+" "+tagValueDglang+" .")
+	// ดึงแต่ละ Input Type Model ไปทำการสร้างคำสั่ง DQL
+	// ที่ใช้สำหรับ Setup Schema ใน Dgraph Database
+	for _, model := range models {
+		v := reflect.ValueOf(model)
+		t := reflect.Indirect(v).Type()
+
+		typeName := t.Name()
+
+		if t.Name() == ModelTypeName {
+			continue
+		}
+
+		numFields := t.NumField()
+
+		for i := 0; i < numFields; i++ {
+			field := t.Field(i)
+			tagValueDglang := field.Tag.Get(TagDgland)
+			tagValueJson := field.Tag.Get(TagJson)
+
+			// หากเป็น Field ที่ไม่มี Json Tag หรือ Dglang Tag
+			//ให้ทำการข้ามรายการ Model Struct นั้นไปเลย
+			if tagValueDglang != "" && tagValueJson != "" {
+				fieldName := strings.Split(tagValueJson, ",")[0]
+				bFields.WriteString(fieldName + ": " + tagValueDglang + " .\n")
+
+				if typeName == ModelTypeName {
+					bModelTypes.WriteString("\t" + fieldName + "\n")
+					if i == numFields-1 {
+						bModelTypes.WriteString("}\n")
+					}
+				} else {
+					if i == 1 {
+						bTypes.WriteString("\ntype " + typeName + " {\n")
+					}
+
+					bTypes.WriteString("\t" + fieldName + "\n")
+
+					if i == numFields-1 {
+						bTypes.WriteString(bModelTypes.String())
+					}
+				}
+			}
 		}
 	}
-	return strings.Join(strs, "\n")
+	return bFields.String() + bTypes.String()
 }
--- utils_test.go
+++ utils_test.go
@@ -4,22 +4,22 @@ import (
 	"context"
 	"github.com/stretchr/testify/suite"
 	"testing"
-	"time"
 )
 
 type User struct {
-	Uid       string    `json:"uid,omitempty"`
-	DType     []string  `json:"dgraph.type,omitempty"`
-	CreatedAt time.Time `json:"created_at,omitempty" dgland:"string"`
-	UpdatedAt time.Time `json:"updated_at,omitempty" dgland:"string"`
-	DeletedAt time.Time `json:"deleted_at,omitempty" dgland:"string"`
-	Username  string    `json:"username,omitempty" dgland:"string @index(exact) @upsert"`
-	Password  string    `json:"password,omitempty" dgland:"password"`
+	Model
+	Username string `json:"username,omitempty" dgland:"string @index(exact) @upsert"`
+	Password string `json:"password,omitempty" dgland:"password"`
+}
+
+type Post struct {
+	Model
+	Title       string `json:"title,omitempty" dgland:"string @index(exact) @upsert"`
+	Description string `json:"description,omitempty" dgland:"string"`
 }
 
 type UtilsTestSuite struct {
 	suite.Suite
-	UserModel    User
 	SchemaResult string
 }
 
@@ -27,16 +27,27 @@ func (suite *UtilsTestSuite) SetupTest() {
 	suite.SchemaResult = `created_at: string .
 updated_at: string .
 deleted_at: string .
-username: string @index(hash) @upsert .
+username: string @index(exact) @upsert .
 password: password .
+title: string @index(exact) @upsert .
+description: string .
 
 type User {
+	username
+	password
 	created_at
 	updated_at
 	deleted_at
-	username
-	password
-}`
+}
+
+type Post {
+	title
+	description
+	created_at
+	updated_at
+	deleted_at
+}
+`
 }
 
 func (suite *UtilsTestSuite) TestCtxFunc() {
@@ -44,7 +55,7 @@ func (suite *UtilsTestSuite) TestCtxFunc() {
 }
 
 func (suite *UtilsTestSuite) TestSchemaFunc() {
-	suite.Equal(suite.SchemaResult, Schema(suite.UserModel))
+	suite.Equal(suite.SchemaResult, Schema(&User{}, &Post{}))
 }
 
 func TestUtilsTestSuite(t *testing.T) {
